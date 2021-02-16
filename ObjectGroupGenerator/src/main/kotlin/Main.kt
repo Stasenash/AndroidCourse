20813c5ab9a08e876ca91bb25cfbf16084a1e9d6
Antonina Bahareva <astasenash@gmail.com>
2021-02-16 21:52:31 +0500

    Some fixes for task1

--- ObjectGroupGenerator/src/main/kotlin/Main.kt
+++ ObjectGroupGenerator/src/main/kotlin/Main.kt
@@ -9,10 +9,7 @@ fun main() {
 
     for (i in 0 until unicornGroup.count()){
         println("Единорог №" + (i + 1))
-        println("Имя: " + unicornGroup[i].name)
-        println("Магический рог: " + if (unicornGroup[i].hasMagicHorn) "есть" else "отсутствует (обычный)")
-        println("Длина рога: " + "%.2f".format(unicornGroup[i].hornLength) + " см")
-        println("Сила радуги: " + unicornGroup[i].rainbowPower + " рогопоинтов")
+        println(unicornGroup[i])
         println()
     }
 }
--- ObjectGroupGenerator/src/main/kotlin/Objects/Unicorn.kt
+++ ObjectGroupGenerator/src/main/kotlin/Objects/Unicorn.kt
@@ -5,4 +5,12 @@ class Unicorn constructor(name: String, hasMagicHorn: Boolean, hornLength: Doubl
     var hasMagicHorn = hasMagicHorn
     var hornLength = hornLength
     var rainbowPower = rainbowPower
+
+    override fun toString(): String {
+        val unicornStr = "Имя: " + name + "\n" +
+                         "Магический рог: " + if (hasMagicHorn) "есть" else "отсутствует (обычный)" + "\n" +
+                         "Длина рога: " + "%.2f".format(hornLength) + " см" + "\n" +
+                         "Сила радуги: " + rainbowPower + " рогопоинтов"
+        return unicornStr
+    }
 }

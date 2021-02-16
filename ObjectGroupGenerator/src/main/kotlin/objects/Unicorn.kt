2e403ee1ed7edd7497fcb1e6dc985b494c02bed4
Antonina Bahareva <astasenash@gmail.com>
2021-02-16 23:44:13 +0500

    Some fixes for task1

--- ObjectGroupGenerator/src/main/kotlin/objects/Unicorn.kt
+++ ObjectGroupGenerator/src/main/kotlin/objects/Unicorn.kt
@@ -3,8 +3,9 @@ package main.kotlin.objects
 class Unicorn constructor(val name: String, val hasMagicHorn: Boolean, val hornLength: Double, val rainbowPower: Int){
 
     override fun toString(): String {
+        val magicHornText = if (hasMagicHorn) "есть" else "отсутствует (обычный)"
         val unicornStr = "Имя: " + name + "\n" +
-                         "Магический рог: " + if (hasMagicHorn) "есть" else "отсутствует (обычный)" + "\n" +
+                         "Магический рог: " + magicHornText + "\n" +
                          "Длина рога: " + "%.2f".format(hornLength) + " см" + "\n" +
                          "Сила радуги: " + rainbowPower + " рогопоинтов"
         return unicornStr
